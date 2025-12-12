0b5918b986d9259c0cee969a2a2f1ccddd328f5a
jayeshjeh <jay.parmar.11169@gmail.com>
2025-12-12 11:52:14 +0530

    bucket-name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-unique-123"
+    bucket         = "my-terraform-state-bucket-unique-122"
     key            = "challenge/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
